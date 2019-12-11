db825a83f0a7db3d63df7e95deeb43c7a33a07bc
tom <tompollak1000@gmail.com>
2019-12-11 15:08:47 +0000

    made urls more readable

--- accountsettings/urls.py
+++ accountsettings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('profile/<str:username>/', views.profile, name='profile'),
+     path('<str:username>/', views.profile, name='profile'),
 ]
--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('leaderboard', views.leaderboard, name='leaderboard'),
+     path('', views.leaderboard, name='leaderboard'),
 ]
