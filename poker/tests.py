b706a9802231791c6261db6203f3e9f9ea41f538
tom <tompollak1000@gmail.com>
2020-01-26 17:50:19 +0000

    bit more finished implementation

--- poker/tests.py
+++ poker/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from tables.models import Table
 
-# Create your tests here.
+class PokerTestCase(TestCase):
+    @classmethod
+    def setup(cls):
+        table = Table.objects.create(name='test_table', buyIn=100, maxNoOfPlayers=4)
+        #num players
+        for a in range(4):
+            name = 'test_' + str(a)
+            pokerThread = threading.Thread(target=tests, args=(table.pk, name), daemon=True)
+            pokerThread.start()
