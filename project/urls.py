94e4232fe598ab5d8a2f6db5784ac4bdfe0d6e47
tom <tompollak1000@gmail.com>
2020-03-14 19:07:22 +0000

    ALOT

--- project/urls.py
+++ project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('how-to-play/', include('rules.urls')),
 ]
