b06acd082569aaaa555568f1d5c4f9eba629ff98
Tom Pollak <tompollak1000@gmail.com>
2020-05-10 13:34:33 +0100

    added procfile

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,3 +157,5 @@ REST_FRAMEWORK = {
     'PAGINATE_BY': 10
 }
 DOCKER_HOST = '192.168.99.100:2376'
+
+django_heroku.settings(locals())
