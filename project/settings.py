b071970ec59b6e5625f2b5f960d4643ea93ea4b9
Tom Pollak <tompollak1000@gmail.com>
2020-05-10 15:01:10 +0100

    stup heroku

--- project/settings.py
+++ project/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
