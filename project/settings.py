ab3d53d149cfda800afb3f8fc0f6f9ca28ed8eaf
Tom Pollak <tompollak1000@gmail.com>
2020-05-10 14:47:08 +0100

    stup heroku

--- project/settings.py
+++ project/settings.py
@@ -86,10 +86,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER'
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
