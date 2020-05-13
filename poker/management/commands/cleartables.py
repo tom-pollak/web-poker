a1544d4afbee0b68c268c1954ba0eca03f4cb055
Tom Pollak <tompollak1000@gmail.com>
2020-05-13 21:21:26 +0100

    added table import

--- poker/management/commands/cleartables.py
+++ poker/management/commands/cleartables.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from poker.models import Players, Room
+from tables.models import Table
 import time
 from datetime import datetime, timezone
 
