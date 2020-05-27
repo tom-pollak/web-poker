d236f736bf37426302c40632d86e1b17fc751967
Tom Pollak <tompollak1000@gmail.com>
2020-05-27 01:13:01 +0100

    back to procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/start-pgbouncer daphne project.asgi:application --port $PORT --bind 0.0.0.0
--- run.sh
+++
@@ -1,6 +0,0 @@
-set -ev
-
-service pgbouncer start
-
-daphne project.asgi:application --port $PORT --bind 0.0.0.0
-service pgbouncer stop
